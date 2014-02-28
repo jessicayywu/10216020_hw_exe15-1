89e52ff198948f85c7e57bcb556d5d3e93d29352
Yen-Yi Wu <jessicayywu@yahoo.com.tw>
2014-02-28 14:13:02 +0800

    The First Version with a Test Program
    to test the first version
    The correct answer is displayed.

---
+++ src/TestAbstractCalculateFunction.java
@@ -0,0 +1,11 @@
+
+public class TestAbstractCalculateFunction {
+
+	public static void main(String[] args) {
+		Function1 function1 = new Function1(); 
+		
+		System.out.println("x = " + function1.f(5));
+
+	}
+
+}
